a85ff01f09783cc3da9ad93436dd19216bdd4a30
shaoyihe <wubuqilai@gmail.com>
2017-08-06 21:25:43 +0800

    优化

--- src/main/java/stone/LocalVarExpr.java
+++ src/main/java/stone/LocalVarExpr.java
@@ -16,13 +16,16 @@ public class LocalVarExpr extends ASTList {
     }
 
     public BinaryExpr assign() {
+        if (!(child(0) instanceof BinaryExpr)) {
+            throw new ParseException("require binary but got " + child(0));
+        }
         return (BinaryExpr) child(0);
     }
 
     @Override
     public Object eval(Environment environment) {
         if (!"=".equals(assign().operator())) {
-            throw new ParseException("require = but got " + assign());
+            throw new ParseException("require = but got " + assign().operator());
         }
         return assign().assign(environment, true);
     }
